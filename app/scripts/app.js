d5de3e209eeb1350220213eb20c2118934d859c3
funnymitrofunny <funnymitrofunny@gmail.com>
2017-11-14 17:25:40 +0500

    add js to get json data

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,26 @@
-// require('bootstrap');
+import $ from 'jquery';
 
-// import '../libs/bootstrap-4.0.0-beta.2/js/src/index.js'
-// import $ from 'jquery';
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
 
-// import '../blocks/place/place.js';
-// import '../blocks/choose-city/choose-city.js';
+		var recipe = JSON.parse(this.responseText);
+
+		$('.recipe__title').html(recipe.title);
+
+		var components = [];
+		for (var i = 0; i < recipe.components.length; i++) {
+			components.push('<li class="recipe__item">' + recipe.components[i] + '</li>');
+		}
+		$('.recipe__components').append(components);
+
+		var steps = [];
+		for (var i = 0; i < recipe.steps.length; i++) {
+			steps.push('<p class="recipe__step">' + recipe.steps[i] + '</p');
+		}
+		$('.recipe__details').append(steps);
+
+	}
+};
+xmlhttp.open("GET", "assets/data/recipe.json");
+xmlhttp.send();
