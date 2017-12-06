73254f3c06cac97b427be54a9b4acb9b4db8f982
funnymitrofunny <funnymitrofunny@gmail.com>
2017-12-06 17:15:30 +0500

    add functions to show/hide blocks

--- app/blocks/nav/nav.js
+++ app/blocks/nav/nav.js
@@ -1,18 +1,60 @@
 import $ from 'jquery';
 
-$('.nav__button').on('click', toggleMobileNav);
-$('.nav__item').on('click', toggleMobileNav);
-$(window).on('resize', showNav);
+$(document).ready(function(){
 
-function toggleMobileNav() {
-	if (window.matchMedia('(max-width: 767px)').matches) {
-		$('.nav__collapse').toggle();
-	}
-}
+	$('.nav__all-recipes').on('click', showAllRecipes);
+	$('.nav__events').on('click', showEvents);
+	$('.nav__try-new').on('click', showTryNew);
+	$('.nav__add-recipe').on('click', showAddRecipe);
+	$('.nav__button').on('click', toggleMobileNav);
+	$('.nav__item').on('click', toggleMobileNav);
+	$(window).on('resize', showLargeNav);
 
-// Make nav to be visible after resizing to large screen
-function showNav() {
-	if (window.matchMedia('(min-width: 768px)').matches) {
-		$('.nav__collapse').show();
+	function showAllRecipes() {
+		$('.events').hide();
+		$('.try-new').hide();
+		$('.add-recipe').hide();
+		$('.all-recipes__breadcrumb').remove();
+		$('.dish-group_opened').remove();
+		$('.all-recipes__dish-group').show();
 	}
-}
+
+	function showEvents() {
+		$('.all-recipes').hide();
+		$('.try-new').hide();
+		$('.add-recipe').hide();
+		$('.events').show();
+	}
+
+	function showTryNew() {
+		$('.all-recipes').hide();
+		$('.events').hide();
+		$('.add-recipe').hide();
+		$('.try-new').show();
+	}
+
+	function showAddRecipe() {
+		$('.all-recipes').hide();
+		$('.events').hide();
+		$('.try-new').hide();
+		$('.add-recipe').show();
+	}
+
+	function toggleMobileNav() {
+		// if (window.matchMedia('(max-width: 767px)').matches) {
+		if (window.innerWidth <= 767) {
+			$('.nav__collapse').toggle();
+		}
+	}
+
+	// Make nav to be visible after resizing to large screen
+	function showLargeNav() {
+		// if (window.matchMedia('(min-width: 768px)').matches) {
+		if (window.innerWidth >= 768) {
+			$('.nav__collapse').show();
+		}
+		else
+			$('.nav__collapse').hide();
+	}
+
+});
