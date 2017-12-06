123da384800a3fc10c9771a8281fafd6e5e47f22
funnymitrofunny <funnymitrofunny@gmail.com>
2017-12-06 16:35:22 +0500

    move breadcrumb functions to breadcrumb folder

---
+++ app/blocks/breadcrumb/breadcrumb.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+
+$(document).ready(function() {
+
+	$(document).on('click', '.breadcrumb__all-dish-groups', backToAllGroups);
+	$(document).on('click', '.breadcrumb__current-dish-group', backToCurrentGroup);
+
+})
+
+function backToAllGroups() {
+	$('.dish-group_opened').remove();
+	$('.all-recipes__breadcrumb').remove();
+	$('.dish-group').show();
+}
+
+function backToCurrentGroup() {
+	$('.recipe').remove();
+	$('.breadcrumb__current-recipe').prev().remove();
+	$('.breadcrumb__current-recipe').remove();
+	$('.recipe-preview').show();
+}
