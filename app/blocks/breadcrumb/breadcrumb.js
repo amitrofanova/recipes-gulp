b5782f96caf91e2c7251d39a913b655a224ba3a0
funnymitrofunny <funnymitrofunny@gmail.com>
2017-12-13 12:26:40 +0500

    move function declaration to .ready()

--- app/blocks/breadcrumb/breadcrumb.js
+++ app/blocks/breadcrumb/breadcrumb.js
@@ -5,17 +5,17 @@ $(document).ready(function() {
 	$(document).on('click', '.breadcrumb__all-dish-groups', backToAllGroups);
 	$(document).on('click', '.breadcrumb__current-dish-group', backToCurrentGroup);
 
-})
+	function backToAllGroups() {
+		$('.dish-group_opened').remove();
+		$('.all-recipes__breadcrumb').remove();
+		$('.dish-group').show();
+	}
 
-function backToAllGroups() {
-	$('.dish-group_opened').remove();
-	$('.all-recipes__breadcrumb').remove();
-	$('.dish-group').show();
-}
+	function backToCurrentGroup() {
+		$('.recipe').remove();
+		$('.breadcrumb__current-recipe').prev().remove();
+		$('.breadcrumb__current-recipe').remove();
+		$('.recipe-preview').show();
+	}
 
-function backToCurrentGroup() {
-	$('.recipe').remove();
-	$('.breadcrumb__current-recipe').prev().remove();
-	$('.breadcrumb__current-recipe').remove();
-	$('.recipe-preview').show();
-}
+});
