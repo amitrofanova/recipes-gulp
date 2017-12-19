5cdf11d928cfae63bf04f089e2dee15bacb73cca
funnymitrofunny <funnymitrofunny@gmail.com>
2017-12-19 21:35:46 +0500

    add resetForm() to be executed when leaving new-recipe form

--- app/blocks/nav/nav.js
+++ app/blocks/nav/nav.js
@@ -1,6 +1,12 @@
 import $ from 'jquery';
+import { resetForm } from '../add-recipe/add-recipe.js';
 
-$(document).ready(function(){
+// window size at which need to toggle from mobile to desktop version
+const windowBreakpoint = 768;
+
+
+$(document).ready(function() {
+
 	toggleNavButton();
 	$('.nav__all-recipes').on('click', showAllRecipes);
 	$('.nav__events').on('click', showEvents);
@@ -11,62 +17,73 @@ $(document).ready(function(){
 	$(window).on('resize', showLargeNav);
 	$(window).on('resize', toggleNavButton);
 
-	function showAllRecipes() {
-		$('.events').hide();
-		$('.try-new').hide();
-		$('.add-recipe').hide();
-		$('.all-recipes__breadcrumb').remove();
-		$('.dish-group_opened').remove();
-		$('.all-recipes').show();
-		$('.all-recipes__dish-group').show();
-	}
-
-	function showEvents() {
-		$('.all-recipes').hide();
-		$('.try-new').hide();
-		$('.add-recipe').hide();
-		$('.events').show();
-	}
-
-	function showTryNew() {
-		$('.all-recipes').hide();
-		$('.events').hide();
-		$('.add-recipe').hide();
-		$('.try-new').show();
-	}
-
-	function showAddRecipe() {
-		$('.all-recipes').hide();
-		$('.events').hide();
-		$('.try-new').hide();
-		$('.add-recipe').show();
-	}
-
-	function toggleNavButton() {
-		if ($('.nav__collapse').css('display') === 'none') {
-			$('.nav__button').addClass('nav__button_open');
-			$('.nav__button').removeClass('nav__button_close');
-		}
-		else if ($('.nav__collapse').css('display') === 'block') {
-			$('.nav__button').addClass('nav__button_close');
-			$('.nav__button').removeClass('nav__button_open');
-		}
-	}
-
-	function toggleMobileNav() {
-		if (window.innerWidth <= 767) {
-			$('.nav__collapse').toggle();
-		}
-		toggleNavButton();
-	}
-
-	// Make nav to be visible after resizing to large screen
-	function showLargeNav() {
-		if (window.innerWidth >= 768) {
-			$('.nav__collapse').show();
-		}
-		else
-			$('.nav__collapse').hide();
-	}
-
 });
+
+
+function showAllRecipes() {
+	$('.events').hide();
+	$('.try-new').hide();
+	$('.add-recipe').hide();
+	resetForm();
+	$('.all-recipes__breadcrumb').remove();
+	$('.dish-group_opened').remove();
+	$('.all-recipes').show();
+	$('.all-recipes__dish-group').show();
+}
+
+
+function showEvents() {
+	$('.all-recipes').hide();
+	$('.try-new').hide();
+	$('.add-recipe').hide();
+	resetForm();
+	$('.events').show();
+}
+
+
+function showTryNew() {
+	$('.all-recipes').hide();
+	$('.events').hide();
+	$('.add-recipe').hide();
+	resetForm();
+	$('.try-new').show();
+}
+
+
+function showAddRecipe() {
+	$('.all-recipes').hide();
+	$('.events').hide();
+	$('.try-new').hide();
+	$('.add-recipe').show();
+}
+
+
+function toggleNavButton() {
+	if ($('.nav__collapse').css('display') === 'none') {
+		$('.nav__button').addClass('nav__button_open');
+		$('.nav__button').removeClass('nav__button_close');
+	}
+	else if ($('.nav__collapse').css('display') === 'block') {
+		$('.nav__button').addClass('nav__button_close');
+		$('.nav__button').removeClass('nav__button_open');
+	}
+}
+
+
+function toggleMobileNav() {
+	if (window.innerWidth < windowBreakpoint) {
+		$('.nav__collapse').toggle();
+	}
+	toggleNavButton();
+}
+
+
+// Make nav to be visible after resizing to large screen
+function showLargeNav() {
+	if (window.innerWidth >= windowBreakpoint) {
+		$('.nav__collapse').show();
+	}
+	else {
+		$('.nav__collapse').hide();
+	}
+}
