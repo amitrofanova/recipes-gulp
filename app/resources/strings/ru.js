4bbb709d9185d7cc63171b33f024eb4e3ecaccfb
funnymitrofunny <funnymitrofunny@gmail.com>
2017-12-19 21:33:02 +0500

    add separate js for string constants

---
+++ app/resources/strings/ru.js
@@ -0,0 +1,10 @@
+// Form constants
+export const UNABLE_LOAD_FILE_ALERT = 'Не удалось загрузить файл';
+export const EMPTY_INGREDIENT_ALERT = 'Укажите ингредиент';
+export const INITIAL_RESULT_TITLE = 'Результат:';
+export const RESULT_ALERT = 'Скопируйте результат в группу ';
+
+
+// Full recipe constants
+export const INGREDIENTS_TITLE = 'Что понадобится:';
+export const STEPS_TITLE = 'Как готовить:';
