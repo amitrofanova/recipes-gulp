dcca907f754c4d1f4e19d65653a905194ec0d689
funnymitrofunny <funnymitrofunny@gmail.com>
2018-01-03 11:18:52 +0500

    add const for successfully added recipe alert

--- app/resources/strings/ru.js
+++ app/resources/strings/ru.js
@@ -3,7 +3,7 @@ export const UNABLE_LOAD_FILE_ALERT = 'Не удалось загрузить ф
 export const EMPTY_INGREDIENT_ALERT = 'Укажите ингредиент';
 export const INITIAL_RESULT_TITLE = 'Результат:';
 export const RESULT_ALERT = 'Скопируйте результат в группу ';
-
+export const ADDED_RECIPE_ALERT = 'Рецепт сохранён!';
 
 // Full recipe constants
 export const INGREDIENTS_TITLE = 'Что понадобится:';
