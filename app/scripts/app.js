17cb174dac38304b3c9ab507f57e75cf9b83b255
funnymitrofunny <funnymitrofunny@gmail.com>
2018-01-03 11:20:05 +0500

    added import of scripts from new blocks

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,6 @@
 import '../blocks/nav/nav.js';
 import '../blocks/all-recipes/all-recipes.js';
 import '../blocks/breadcrumb/breadcrumb.js';
+import '../blocks/dashboard/dashboard.js';
 import '../blocks/add-recipe/add-recipe.js';
+import '../blocks/delete-recipe/delete-recipe.js';
