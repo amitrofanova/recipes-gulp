7a11ed73122b1e9791aabd196e79e12f7a5d3649
funnymitrofunny <funnymitrofunny@gmail.com>
2018-01-08 14:04:16 +0500

    add function to get select options from json

--- app/blocks/delete-recipe/delete-recipe.js
+++ app/blocks/delete-recipe/delete-recipe.js
@@ -2,12 +2,68 @@ import $ from 'jquery';
 
 
 const jsonPath = 'https://amitrofanova.pythonanywhere.com/api/recipes';
+const pathToNames = 'https://amitrofanova.pythonanywhere.com/api/recipes/names';
+
+
+function getList(callback, group) {
+	const result = [];
+	let url = pathToNames;
+
+	if (group) {
+		url += '?group=' + group;
+	}
+
+	$.ajax({
+		url,
+		dataType: 'json',
+		success(data){
+			callback(data);
+		}
+	});
+
+	return result;
+}
+
+
+function createGroupsSelect() {
+	const callback = function (data) {
+		for (let i = 0; i < data.length; i++) {
+			$('.delete-recipe__dish-group').append(
+				'<option value="' + data[i].group + '">' + data[i].group + '</option>'
+			);
+		}
+	};
+	getList(callback);
+}
+
+
+function clearRecipesSelect() {
+	$('.delete-recipe__recipe-to-delete').text('');
+}
+
+
+function createRecipesSelect() {
+	clearRecipesSelect();
+
+	const selectedGroup = '.delete-recipe__dish-group option:selected';
+	const group = $(selectedGroup).text();
+
+	const callback = function (data) {
+		for (let i = 0; i < data.recipes.length; i++) {
+			$('.delete-recipe__recipe-to-delete').append(
+				'<option value="' + data.recipes[i] + '">' + data.recipes[i] + '</option>'
+			);
+		}
+	};
+	getList(callback, group);
+}
 
 
 function deleteRecipe() {
 	const selectedGroup = '.delete-recipe__dish-group option:selected';
 	const group = $(selectedGroup).text();
-	const recipeToDelete = $('.delete-recipe__recipe-to-delete').val();
+	const selectedRecipe = '.delete-recipe__recipe-to-delete option:selected';
+	const recipeToDelete = $(selectedRecipe).text();
 	const url = jsonPath + '?group=' + group + '&recipe=' + recipeToDelete;
 
 	$.ajax({
@@ -26,5 +82,9 @@ function deleteRecipe() {
 
 
 $(document).ready(function (){
+
+	createGroupsSelect();
+	$(document).on('change', '.delete-recipe__dish-group', createRecipesSelect);
 	$(document).on('click', '.delete-recipe__delete-recipe', deleteRecipe);
+
 });
