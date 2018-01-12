28eaee3c8689354a5aeafd17e0a318da827cee07
funnymitrofunny <funnymitrofunny@gmail.com>
2018-01-12 13:39:46 +0500

    add module "modal-alert"

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,7 @@
 import '../blocks/nav/nav.js';
 import '../blocks/all-recipes/all-recipes.js';
 import '../blocks/breadcrumb/breadcrumb.js';
+import '../blocks/modal-alert/modal-alert.js';
 import '../blocks/dashboard/dashboard.js';
 import '../blocks/add-recipe/add-recipe.js';
 import '../blocks/delete-recipe/delete-recipe.js';
