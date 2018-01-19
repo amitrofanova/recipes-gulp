e106c8013d2e624d4fc9a82baa8e52e9360bf7de
funnymitrofunny <funnymitrofunny@gmail.com>
2018-01-19 11:49:54 +0500

    add "confirmation" functionality to alert

--- app/blocks/modal-alert/modal-alert.js
+++ app/blocks/modal-alert/modal-alert.js
@@ -1,16 +1,31 @@
 import $ from 'jquery';
 
 
-export function showAlert(form, alert) {
-	form
-		.append(
-			'<div class="modal-alert">' +
-				'<div class="modal-alert__content">' +
-					'<div class="modal-alert__text">' + alert + '</div>' +
-					'<div class="modal-alert__close-btn"></div>' +
-				'</div>' +
-			'</div>'
-		);
+export function showAlert(alertText, isConfirmation) {
+	if (isConfirmation) {
+		$('body')
+			.append(
+				'<div class="modal-alert">' +
+					'<div class="modal-alert__content">' +
+						'<div class="modal-alert__text">' + alertText + '</div>' +
+						'<div class="modal-alert__close-btn"></div>' +
+						'<div class="modal-alert__confirm-btn">Да</div>' +
+						'<div class="modal-alert__decline-btn">Отмена</div>' +
+					'</div>' +
+				'</div>'
+			);
+	}
+	else {
+		$('body')
+			.append(
+				'<div class="modal-alert">' +
+					'<div class="modal-alert__content">' +
+						'<div class="modal-alert__text">' + alertText + '</div>' +
+						'<div class="modal-alert__close-btn"></div>' +
+					'</div>' +
+				'</div>'
+			);
+	}
 }
 
 
