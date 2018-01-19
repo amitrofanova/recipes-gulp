7b8586ca115660293a9eaa422530c6d27ee91a90
funnymitrofunny <funnymitrofunny@gmail.com>
2018-01-19 16:44:22 +0500

    add constants for login/registration page messages

---
+++ app/resources/strings/en.js
@@ -0,0 +1,12 @@
+// Login and register forms constants
+export const LOGIN_TAB_TEXT = 'Login';
+export const REGISTER_TAB_TEXT = 'Register';
+export const LOGIN_HEADER_TEXT = 'Welcome to Recipe Book';
+export const LOGIN_SUB_HEADER_TEXT = 'Your own recipes collection';
+export const USERNAME_PLACEHOLDER = 'Username';
+export const PASSWORD_PLACEHOLDER = 'Password';
+export const LOGIN_BUTTON_TEXT = 'Login';
+export const REGISTER_HEADER_TEXT = 'Are you new to Recipe Book?';
+export const REGISTER_SUB_HEADER_TEXT = 'Sign up with email';
+export const EMAIL_PLACEHOLDER = 'Email';
+export const SIGNUP_BUTTON_TEXT = 'Sign up';
--- app/resources/strings/ru.js
+++ app/resources/strings/ru.js
@@ -1,12 +1,32 @@
-// Form constants
+// Register form constants
+export const INVALID_EMAIL_ERR = 'Неверный адрес электронной почты';
+export const SHORT_PWD_ERR = 'Пароль слишком короткий';
+export const NOT_EQUAL_PWD_ERR = 'Пароли должны совпадать';
+export const USER_REGISTERED_ALERT = 'Вы успешно зарегистрированы!';
+
+// Login form constants
+export const INVALID_LOGIN_ERR = 'Неверное имя пользователя или пароль';
+
+// export const LOGIN_TAB_TEXT = 'Вход';
+// export const REGISTER_TAB_TEXT = 'Регистрация';
+// export const LOGIN_HEADER_TEXT = 'Добро пожаловать в Книгу Рецептов';
+// export const LOGIN_SUB_HEADER_TEXT = 'Ваша личная коллекция рецептов';
+// export const USERNAME_PLACEHOLDER = 'Имя пользователя';
+// export const PASSWORD_PLACEHOLDER = 'Пароль';
+// export const LOGIN_BUTTON_TEXT = 'Войти';
+// export const REGISTER_HEADER_TEXT = 'Впервые на сайте?';
+// export const REGISTER_SUB_HEADER_TEXT = 'Зарегистрируйтесь с электронной почтой';
+// export const EMAIL_PLACEHOLDER = 'Электронная почта';
+// export const SIGNUP_BUTTON_TEXT = 'Зарегистрироваться';
+
+// Dashboard constants
 export const UNABLE_LOAD_FILE_ALERT = 'Не удалось загрузить файл';
 export const EMPTY_INGREDIENT_ALERT = 'Укажите ингредиент';
-export const INITIAL_RESULT_TITLE = 'Результат:';
-export const RESULT_ALERT = 'Скопируйте результат в группу ';
+export const CONFIRM_DELETE_ALERT = 'Вы уверены, что хотите удалить рецепт?';
 export const ADDED_RECIPE_ALERT = 'Рецепт сохранён!';
 export const DELETED_RECIPE_ALERT = 'Рецепт удалён!';
 export const ERROR_ALERT = 'Произошла ошибка!';
 
-// Full recipe constants
+// A recipe constants
 export const INGREDIENTS_TITLE = 'Что понадобится:';
 export const STEPS_TITLE = 'Как готовить:';
