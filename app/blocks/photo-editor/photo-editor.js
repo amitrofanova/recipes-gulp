d4b34e2ecadd8b0fe21f7b57450cb052290cd79d
funnymitrofunny <funnymitrofunny@gmail.com>
2018-02-14 21:51:12 +0300

    add possibility to generate html for editor on click

--- app/blocks/photo-editor/photo-editor.js
+++ app/blocks/photo-editor/photo-editor.js
@@ -2,6 +2,73 @@ import $ from 'jquery';
 import Cropper from 'cropperjs';
 
 
+export function createEditor() {
+	$(this).after(
+		'<div class="photo-editor">' +
+			'<div class="photo-editor__inner">' +
+				'<div class="photo-editor__tabs">' +
+					'<div class="photo-editor__tab photo-editor__choose-file-tab photo-editor__tab-active">1. Загрузить фото</div>' +
+					'<div class="photo-editor__tab photo-editor__crop-tab">2. Обрезать фото</div>' +
+					'<div class="photo-editor__tab photo-editor__crop-min-tab">3. Выбрать миниатюру</div>' +
+				'</div>' +
+
+				'<section class="photo-editor__section photo-editor__section-active photo-editor__choose-file-section">' +
+					'<div class="photo-editor__title"></div>' +
+					'<div class="photo-editor__description">Загрузите фото с вашим блюдом</div>' +
+					'<div class="photo-editor__section-content">' +
+						'<div class="photo-editor__image-load-wrap">' +
+							'<img class="photo-editor__image-load">' +
+						'</div>' +
+						'<div class="photo-editor__choose-file-btn button">Выбрать изображение' +
+							'<input type="file" name="newfile" class="photo-editor__file-input">' +
+						'</div>' +
+					'</div>' +
+					'<div class="photo-editor__controls">' +
+						'<div class="photo-editor__to-crop-btn photo-editor__btn_right button">Продолжить</div>' +
+					'</div>' +
+				'</section>' +
+
+				'<section class="photo-editor__section photo-editor__crop-section">' +
+					'<div class="photo-editor__title"></div>' +
+					'<div class="photo-editor__description">' +
+						'Пожалуйста обрежьте фото. Фото будет сохранено в пропорции 16:9. ' +
+						'Такая пропорция нужна, чтобы ваше фото лучше смотрелось на странице :)' +
+					'</div>' +
+					'<div class="photo-editor__section-content">' +
+						'<div class="photo-editor__image-crop-wrap">' +
+							'<img class="photo-editor__image-crop">' +
+						'</div>' +
+						'<div class="photo-editor__crop-btn button">Обрезать</div>' +
+					'</div>' +
+					'<div class="photo-editor__controls">' +
+						'<div class="photo-editor__to-choose-file-btn photo-editor__btn_left button">Вернуться</div>' +
+						'<div class="photo-editor__to-crop-min-btn photo-editor__btn_right button">Продолжить</div>' +
+					'</div>' +
+				'</section>' +
+
+				'<section class="photo-editor__section photo-editor__crop-min-section">' +
+					'<div class="photo-editor__title"></div>' +
+					'<div class="photo-editor__description">' +
+						'Пожалуйста выберите область изображения, которая будет использоваться для предпросмотра вашего рецепта.' +
+					'</div>' +
+					'<div class="photo-editor__section-content">' +
+						'<div class="photo-editor__crop-min-wrap">' +
+							'<img class="photo-editor__image-crop-min">' +
+						'</div>' +
+						'<div class="photo-editor__crop-min-btn button">Обрезать</div>' +
+					'</div>' +
+					'<div class="photo-editor__controls">' +
+						'<div class="photo-editor__to-crop-btn photo-editor__btn_left button">Вернуться</div>' +
+						'<div class="photo-editor__submit-btn photo-editor__btn_right button">Готово</div>' +
+					'</div>' +
+				'</section>' +
+
+			'</div>' +
+		'</div>'
+	);
+}
+
+
 function cropImage() {
 	const image = $('.photo-editor__image-crop')[0];
 	const cropper = new Cropper(image, {
@@ -12,7 +79,7 @@ function cropImage() {
 		minContainerHeight: 180
 	});
 
-	$('.photo-editor__crop-btn').on('click', function () {
+	$(document).on('click', '.photo-editor__crop-btn', function () {
 		const res = cropper.getCroppedCanvas({maxWidth: 1600, maxHeight: 900}).toDataURL();
 		cropper.destroy();
 		$('.photo-editor__image-crop').attr('src', res);
@@ -30,7 +97,7 @@ function cropImageMin() {
 		minContainerHeight: 180
 	});
 
-	$('.photo-editor__crop-min-btn').on('click', function () {
+	$(document).on('click', '.photo-editor__crop-min-btn', function () {
 		const res = cropper.getCroppedCanvas({maxWidth: 1600, maxHeight: 900}).toDataURL();
 		cropper.destroy();
 		$('.photo-editor__image-crop-min').attr('src', res);
@@ -95,21 +162,21 @@ $(document).ready(function () {
 
 	$(document).on('change', '.photo-editor__file-input', uploadImage);
 
-	$('.photo-editor__to-crop-btn').on('click', function () {
+	$(document).on('click', '.photo-editor__to-crop-btn', function () {
 		$('.photo-editor__section').removeClass('photo-editor__section-active');
 		$('.photo-editor__crop-section').addClass('photo-editor__section-active');
 		$('.photo-editor__tab').removeClass('photo-editor__tab-active');
 		$('.photo-editor__crop-tab').addClass('photo-editor__tab-active');
 	});
 
-	$('.photo-editor__to-choose-file-btn').on('click', function () {
+	$(document).on('click', '.photo-editor__to-choose-file-btn', function () {
 		$('.photo-editor__section').removeClass('photo-editor__section-active');
 		$('.photo-editor__choose-file-section').addClass('photo-editor__section-active');
 		$('.photo-editor__tab').removeClass('photo-editor__tab-active');
 		$('.photo-editor__choose-file-tab').addClass('photo-editor__tab-active');
 	});
 
-	$('.photo-editor__to-crop-min-btn').on('click', function () {
+	$(document).on('click', '.photo-editor__to-crop-min-btn', function () {
 		$('.photo-editor__section').removeClass('photo-editor__section-active');
 		$('.photo-editor__crop-min-section').addClass('photo-editor__section-active');
 		$('.photo-editor__tab').removeClass('photo-editor__tab-active');
