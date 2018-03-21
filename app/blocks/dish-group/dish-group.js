4d58f74c06b19e39380d392590578d55cf8df1d4
funnymitrofunny <funnymitrofunny@gmail.com>
2018-03-21 14:41:51 +0400

    linter changes

--- app/blocks/dish-group/dish-group.js
+++ app/blocks/dish-group/dish-group.js
@@ -1,7 +1,7 @@
 import $ from "jquery";
 
 $(document).ready(function (){
-	$(".dish-group__img, .dish-group__title").hover(function() {
+	$(".dish-group__img, .dish-group__title").hover(function () {
 		$(this).parent().find(".dish-group__title").toggleClass("dish-group__title_opacity09");
 	});
 });
